8332ce9ef060cc799967e596e0af5b668d694ce4
triznylaras <triznylaras@gmail.com>
2021-10-09 14:33:31 +0700

    replace with passed code from codewars

--- sum-consecutive-number.js
+++ sum-consecutive-number.js
@@ -1,9 +1,5 @@
-function sumConsecutiveNumbers(number, first_number) {
-  let sum = 0
-  if (Number.isInteger(number) && number >= 0) {
-    sum = (number / 2) * (first_number + number)
-    return sum
+function f(n){
+  if (Number.isInteger(n) && n > 0) {
+    return sum = (n / 2) * (1 + n)
   } else { return false }
-}
-
-console.log(sumConsecutiveNumbers(10, 1))
+};
